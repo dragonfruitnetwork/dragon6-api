7982f2455d3e9eb1fee2bc74678d5ada7dda354b
Albie Spriddell <albion@dragonfruit.network>
2019-09-25 18:49:12 +0100

    simplify verification level gaining

--- Dragon6-API/Verification/Server.cs
+++ Dragon6-API/Verification/Server.cs
@@ -11,17 +11,20 @@ namespace Dragon6.API.Verification
     {
         internal static IEnumerable<Verification> Users { get; private set; }
 
-        private static bool IsInit = false;
-
         public static async Task Init(string endpoint)
         {
             Users = JsonConvert.DeserializeObject<IEnumerable<Verification>>(await new HttpClient().GetAsync(endpoint).Result.Content.ReadAsStringAsync());
-            IsInit = true;
         }
 
         public static Verification GetUser(string GUID)
         {
-            return IsInit ? Users.SingleOrDefault(x => x.GUID.Equals(GUID, StringComparison.OrdinalIgnoreCase)) : new Verification()
+            try
+            {
+                return Users.Single(x => x.GUID.Equals(GUID, StringComparison.OrdinalIgnoreCase));
+            }
+            catch { }
+
+            return new Verification()
             {
                 GUID = GUID,
                 AccountLevel = Level.Normal
