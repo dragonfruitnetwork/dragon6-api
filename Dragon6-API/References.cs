b4c77064859b2cb6923d3184f0d9d5349eddc1a9
Albie Spriddell <albion@dragonfruit.network>
2019-10-15 20:10:28 +0100

    include new ranks

--- Dragon6-API/References.cs
+++ Dragon6-API/References.cs
@@ -18,32 +18,40 @@ namespace Dragon6.API
             "NCSA"
         };
 
-        public static string GetWeaponClass(int id)
-        {
-            try
-            {
-                return WeaponClasses[id];
-            }
-            catch
-            {
-                return string.Empty;
-            }
-        }
 
-        public static string GetRankName(int id)
+        /// <summary>
+        /// English Versions of Rank ID -> Names (for operations prior to ember rise)
+        /// </summary>
+        private static readonly Dictionary<int, string> LeagacyRankNames = new Dictionary<int, string>
         {
-            try
-            {
-                return RankNames[id];
-            }
-            catch
-            {
-                if(id > 21)
-                    return RankNames[20]; //temp fix for broken ubi api
-            }
+            {0, "Unranked"},
+            
+            {1, "Copper 4"},
+            {2, "Copper 3"},
+            {3, "Copper 2"},
+            {4, "Copper 1"},
 
-            return RankNames[0];
-        }
+            {5, "Bronze 4"},
+            {6, "Bronze 3"},
+            {7, "Bronze 2"},
+            {8, "Bronze 1"},
+
+            {9, "Silver 4"},
+            {10, "Silver 3"},
+            {11, "Silver 2"},
+            {12, "Silver 1"},
+
+            {13, "Gold 4"},
+            {14, "Gold 3"},
+            {15, "Gold 2"},
+            {16, "Gold 1"},
+
+            {17, "Platinum 3"},
+            {18, "Platinum 2"},
+            {19, "Platinum 1"},
+
+            {20, "Diamond"},
+        };
 
         /// <summary>
         /// English Versions of Rank ID -> Names
@@ -51,28 +59,38 @@ namespace Dragon6.API
         private static readonly Dictionary<int, string> RankNames = new Dictionary<int, string>
         {
             {0, "Unranked"},
-            {1, "Copper 4"},
-            {2, "Copper 3"},
-            {3, "Copper 2"},
-            {4, "Copper 1"},
-            {5, "Bronze 4"},
-            {6, "Bronze 3"},
-            {7, "Bronze 2"},
-            {8, "Bronze 1"},
-            {9, "Silver 4"},
-            {10, "Silver 3"},
-            {11, "Silver 2"},
-            {12, "Silver 1"},
-            {13, "Gold 4"},
-            {14, "Gold 3"},
-            {15, "Gold 2"},
-            {16, "Gold 1"},
-            {17, "Platinum 3"},
-            {18, "Platinum 2"},
-            {19, "Platinum 1"},
-            {20, "Diamond"},
-            {21, "Champion"}
+
+            {1, "Copper 5"},
+            {2, "Copper 4"},
+            {3, "Copper 3"},
+            {4, "Copper 2"},
+            {5, "Copper 1"},
+
+            {6, "Bronze 5"},
+            {7, "Bronze 4"},
+            {8, "Bronze 3"},
+            {9, "Bronze 2"},
+            {10, "Bronze 1"},
+
+            {11, "Silver 5"},
+            {12, "Silver 4"},
+            {13, "Silver 3"},
+            {14, "Silver 2"},
+            {15, "Silver 1"},
+
+            {16, "Gold 3"},
+            {17, "Gold 2"},
+            {18, "Gold 1"},
+
+            {19, "Platinum 3"},
+            {20, "Platinum 2"},
+            {21, "Platinum 1"},
+
+            {22, "Diamond"},
+
+            {23, "Champion"}
         };
+
 
         /// <summary>
         /// English Names of Weapon Types
