fe02070a72ba49d8cb379ce0b5a32de1ceb0afc1
Albie <admin@dragonfruit.network>
2019-10-16 07:13:13 +0100

    rank fixes (#14)
    * version updates
    
    * fix platform id mis-sync
    
    * var access fix
    
    * nuget package update
    
    * simplify verification level gaining
    
    * package ver update
    
    * include new ranks
    
    * make dictionaries accessible
    
    * inconsistency fixes

--- Dragon6-API/References.cs
+++ Dragon6-API/References.cs
@@ -18,66 +18,54 @@ namespace Dragon6.API
             "NCSA"
         };
 
-        public static string GetWeaponClass(int id)
-        {
-            try
-            {
-                return WeaponClasses[id];
-            }
-            catch
-            {
-                return string.Empty;
-            }
-        }
-
-        public static string GetRankName(int id)
-        {
-            try
-            {
-                return RankNames[id];
-            }
-            catch
-            {
-                if(id > 21)
-                    return RankNames[20]; //temp fix for broken ubi api
-            }
-
-            return RankNames[0];
-        }
+        /// <summary>
+        /// English Versions of Rank ID -> Names (for operations prior to ember rise)
+        /// </summary>
+        public static readonly Dictionary<int, string> LegacyRankNames = new Dictionary<int, string>
 
         /// <summary>
         /// English Versions of Rank ID -> Names
         /// </summary>
-        private static readonly Dictionary<int, string> RankNames = new Dictionary<int, string>
+        public static readonly Dictionary<int, string> RankNames = new Dictionary<int, string>
         {
             {0, "Unranked"},
-            {1, "Copper 4"},
-            {2, "Copper 3"},
-            {3, "Copper 2"},
-            {4, "Copper 1"},
-            {5, "Bronze 4"},
-            {6, "Bronze 3"},
-            {7, "Bronze 2"},
-            {8, "Bronze 1"},
-            {9, "Silver 4"},
-            {10, "Silver 3"},
-            {11, "Silver 2"},
-            {12, "Silver 1"},
-            {13, "Gold 4"},
-            {14, "Gold 3"},
-            {15, "Gold 2"},
-            {16, "Gold 1"},
-            {17, "Platinum 3"},
-            {18, "Platinum 2"},
-            {19, "Platinum 1"},
-            {20, "Diamond"},
-            {21, "Champion"}
+
+            {1, "Copper 5"},
+            {2, "Copper 4"},
+            {3, "Copper 3"},
+            {4, "Copper 2"},
+            {5, "Copper 1"},
+
+            {6, "Bronze 5"},
+            {7, "Bronze 4"},
+            {8, "Bronze 3"},
+            {9, "Bronze 2"},
+            {10, "Bronze 1"},
+
+            {11, "Silver 5"},
+            {12, "Silver 4"},
+            {13, "Silver 3"},
+            {14, "Silver 2"},
+            {15, "Silver 1"},
+
+            {16, "Gold 3"},
+            {17, "Gold 2"},
+            {18, "Gold 1"},
+
+            {19, "Platinum 3"},
+            {20, "Platinum 2"},
+            {21, "Platinum 1"},
+
+            {22, "Diamond"},
+
+            {23, "Champion"}
         };
+
 
         /// <summary>
         /// English Names of Weapon Types
         /// </summary>
-        internal static readonly Dictionary<int, string> WeaponClasses = new Dictionary<int, string>
+        public static readonly Dictionary<int, string> WeaponClasses = new Dictionary<int, string>
         {
             {1, "Assault Rifle"},
             {2, "SMG"},
