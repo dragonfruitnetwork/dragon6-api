08ce8ddeade70060b3ca4c5cb28826087caf02f0
Albie <albion@dragonfruit.network>
2020-10-13 17:29:21 +0100

    merge UbisoftIdentifiers.cs to single file

--- DragonFruit.Six.API/Helpers/UbisoftIdentifiers.cs
+++ DragonFruit.Six.API/Helpers/UbisoftIdentifiers.cs
@@ -2,12 +2,20 @@
 // Licensed under Apache-2. Please refer to the LICENSE file for more info
 
 using System;
+using System.Collections.Generic;
 using DragonFruit.Six.API.Enums;
 
 namespace DragonFruit.Six.API.Helpers
 {
     public static partial class UbisoftIdentifiers
     {
+        private static IReadOnlyDictionary<Platform, string> _gameIds;
+
+        /// <summary>
+        /// <see cref="IReadOnlyDictionary{TKey,TValue}"/> of platforms to game ids used for checking playtime stats
+        /// </summary>
+        internal static IReadOnlyDictionary<Platform, string> GameIds => _gameIds ??= PlatformParser.PlatformsTo(p => p.GameId());
+
         /// <summary>
         /// Gets the Ubi-AppId for the specified service
         /// </summary>
