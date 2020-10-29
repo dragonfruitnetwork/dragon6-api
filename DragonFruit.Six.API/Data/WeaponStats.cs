0b3ee7e6eaff117f1117a57e34a866f940254a0c
Albie <albion@dragonfruit.network>
2020-10-29 10:39:53 +0000

    add universal naming and fix up operatordata methods

--- DragonFruit.Six.API/Data/WeaponStats.cs
+++ DragonFruit.Six.API/Data/WeaponStats.cs
@@ -3,6 +3,7 @@
 
 using DragonFruit.Six.API.Enums;
 using DragonFruit.Six.API.Utils;
+using Newtonsoft.Json;
 
 namespace DragonFruit.Six.API.Data
 {
@@ -16,46 +17,64 @@ namespace DragonFruit.Six.API.Data
         /// <summary>
         /// Profile Id
         /// </summary>
+        [JsonProperty("id")]
         public string Guid { get; set; }
 
         /// <summary>
         /// <see cref="WeaponType"/> the stats relate to
         /// </summary>
+        [JsonProperty("class")]
         public WeaponType Class { get; set; }
 
         /// <summary>
         /// Total times this class has been picked by the user
         /// </summary>
+        [JsonProperty("picks")]
         public uint TimesChosen { get; set; }
 
+        [JsonProperty("kills")]
         public uint Kills { get; set; }
+
+        [JsonProperty("deaths")]
         public uint Deaths { get; set; }
+
+        [JsonProperty("headshots")]
         public uint Headshots { get; set; }
 
+        [JsonProperty("downs")]
         public uint Downs { get; set; }
+
+        [JsonProperty("down_assists")]
         public uint DownAssists { get; set; }
 
+        [JsonProperty("shots_fired")]
         public uint ShotsFired { get; set; }
+
+        [JsonProperty("shots_landed")]
         public uint ShotsLanded { get; set; }
 
         /// <summary>
         /// Accuracy ratio consisting of <see cref="ShotsLanded"/> to <see cref="ShotsFired"/>
         /// </summary>
+        [JsonProperty("ar")]
         public float Accuracy => _accuracy ??= RatioUtils.RatioOf(ShotsLanded, ShotsFired);
 
         /// <summary>
         /// Efficiency ratio consisting of <see cref="Kills"/> to <see cref="ShotsFired"/>
         /// </summary>
+        [JsonProperty("er")]
         public float Efficiency => _efficiency ??= RatioUtils.RatioOf(Kills, ShotsFired);
 
         /// <summary>
         /// Headshot ratio consisting of <see cref="Headshots"/> to <see cref="ShotsLanded"/>
         /// </summary>
+        [JsonProperty("hr")]
         public float HeadshotRatio => _headshotRatio ??= RatioUtils.RatioOf(Headshots, ShotsLanded);
 
         /// <summary>
         /// Power ratio consisting of <see cref="Kills"/> to <see cref="ShotsLanded"/>
         /// </summary>
+        [JsonProperty("pr")]
         public float Power => _power ??= RatioUtils.RatioOf(Kills, ShotsLanded);
     }
 }
