ab2193d71f77a712f90d9485436a7702457cb6a4
abuZayed15 <abuZayed15>
2020-12-11 12:54:21 -0500

    added new methods to get user by id and name also changed GetUser to FirstOrDefault

--- DragonFruit.Six.API/Data/Extensions/AccountInfoExtensions.cs
+++ DragonFruit.Six.API/Data/Extensions/AccountInfoExtensions.cs
@@ -17,7 +17,7 @@ namespace DragonFruit.Six.API.Data.Extensions
         /// Get a user's account info (in order to get stats)
         /// </summary>
         public static AccountInfo GetUser<T>(this T client, Platform platform, LookupMethod lookupMethod, string query, CancellationToken token = default) where T : Dragon6Client
-            => GetUsers(client, platform, lookupMethod, new[] { query }, token).First();
+            => GetUsers(client, platform, lookupMethod, new[] { query }, token).FirstOrDefault();
 
         /// <summary>
         /// Get multiple users' account info through a mass query search
@@ -27,5 +27,17 @@ namespace DragonFruit.Six.API.Data.Extensions
             var request = new AccountInfoRequest(platform, lookupMethod, queries);
             return client.Perform<JObject>(request, token).DeserializeAccountInfo();
         }
+
+        /// <summary>
+        /// Get a user's account info by name
+        /// </summary>
+        public static AccountInfo GetUserByName<T>(this T client, string name, Platform platform) where T : Dragon6Client
+            => client.GetUser(platform, LookupMethod.Name, name);
+
+        /// <summary>
+        /// Get a user's account info by userId
+        /// </summary>
+        public static AccountInfo GetUserByUbisoftId<T>(this T client, string userId, Platform platform) where T : Dragon6Client
+            => client.GetUser(platform, LookupMethod.UserId, userId);
     }
 }
