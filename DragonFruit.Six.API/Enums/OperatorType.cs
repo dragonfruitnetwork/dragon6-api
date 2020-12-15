88af04d8df3b4cca349315afb2d00f8932f5697a
Albie <albion@dragonfruit.network>
2020-12-15 07:04:52 +0000

    mark operatortype as a flag enum

--- DragonFruit.Six.API/Enums/OperatorType.cs
+++ DragonFruit.Six.API/Enums/OperatorType.cs
@@ -1,8 +1,11 @@
 ﻿// Dragon6 API Copyright 2020 DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under Apache-2. Please refer to the LICENSE file for more info
 
+using System;
+
 namespace DragonFruit.Six.API.Enums
 {
+    [Flags]
     public enum OperatorType
     {
         /// <summary>
