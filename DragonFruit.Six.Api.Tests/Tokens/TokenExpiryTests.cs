e3ac957ff0dc77c650e418458386c464f903ce2e
Albie <albion@dragonfruit.network>
2021-04-25 08:37:44 +0100

    prevent exceptions being thrown with new tokens

---
+++ DragonFruit.Six.Api.Tests/Tokens/TokenExpiryTests.cs
@@ -0,0 +1,19 @@
+﻿// Dragon6 API Copyright 2021 DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under Apache-2. Please refer to the LICENSE file for more info
+
+using DragonFruit.Six.Api.Tokens;
+using NUnit.Framework;
+
+namespace DragonFruit.Six.Api.Tests.Tokens
+{
+    [TestFixture]
+    public class TokenExpiryTests
+    {
+        [Test]
+        public void TestTokenExpiry()
+        {
+            var token = new Dragon6Token();
+            Assert.True(token.Expired);
+        }
+    }
+}
