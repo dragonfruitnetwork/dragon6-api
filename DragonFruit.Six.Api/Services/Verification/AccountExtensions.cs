87f5f09fde1946140ec72d5d13e780702f0851bb
Albie <albion@dragonfruit.network>
2021-06-12 19:26:16 +0100

    expose more extensions

--- DragonFruit.Six.Api/Services/Verification/AccountExtensions.cs
+++ DragonFruit.Six.Api/Services/Verification/AccountExtensions.cs
@@ -3,16 +3,16 @@
 
 namespace DragonFruit.Six.Api.Services.Verification
 {
-    public static class AccountTypeExtensions
+    public static class AccountExtensions
     {
-        public static string GetName(this Dragon6AccountInfo dragon6User) => string.IsNullOrEmpty(dragon6User.CustomTitle) ? GetDefaultName(dragon6User.AccountRole) : dragon6User.CustomTitle;
-        public static string GetIcon(this Dragon6AccountInfo dragon6User) => string.IsNullOrEmpty(dragon6User.CustomIcon) ? GetDefaultIcon(dragon6User.AccountRole) : dragon6User.CustomIcon;
-        public static string GetColor(this Dragon6AccountInfo dragon6User) => string.IsNullOrEmpty(dragon6User.CustomColour) ? GetDefaultColor(dragon6User.AccountRole) : dragon6User.CustomColour;
+        public static string GetName(this Dragon6AccountInfo dragon6User) => string.IsNullOrEmpty(dragon6User.CustomTitle) ? GetName(dragon6User.AccountRole) : dragon6User.CustomTitle;
+        public static string GetIcon(this Dragon6AccountInfo dragon6User) => string.IsNullOrEmpty(dragon6User.CustomIcon) ? GetIcon(dragon6User.AccountRole) : dragon6User.CustomIcon;
+        public static string GetColor(this Dragon6AccountInfo dragon6User) => string.IsNullOrEmpty(dragon6User.CustomColour) ? GetColor(dragon6User.AccountRole) : dragon6User.CustomColour;
 
         /// <summary>
         /// Retrieves the default title for the <see cref="AccountRole"/> provided
         /// </summary>
-        private static string GetDefaultName(this AccountRole role) => role switch
+        public static string GetName(this AccountRole role) => role switch
         {
             AccountRole.BlockedByAdmin => "Account blocked by admin",
             AccountRole.BlockedBySelf => "Account blocked by owner",
@@ -30,7 +30,7 @@ namespace DragonFruit.Six.Api.Services.Verification
         /// <summary>
         /// Retrieves the default icon for the <see cref="AccountRole"/> provided
         /// </summary>
-        private static string GetDefaultIcon(this AccountRole role) => role switch
+        public static string GetIcon(this AccountRole role) => role switch
         {
             AccountRole.BlockedByAdmin => "highlight_off",
             AccountRole.BlockedBySelf => "explore_off",
@@ -46,9 +46,9 @@ namespace DragonFruit.Six.Api.Services.Verification
         };
 
         /// <summary>
-        /// Retrieves the default title colour for the <see cref="AccountRole"/> provided
+        /// Retrieves the default title color for the <see cref="AccountRole"/> provided
         /// </summary>
-        private static string GetDefaultColor(this AccountRole role) => role switch
+        public static string GetColor(this AccountRole role) => role switch
         {
             AccountRole.BlockedByAdmin => "#BD1818",
             AccountRole.BlockedBySelf => "#BD1818",
