ef2c82f8d34cae622fd004e91755faaed3bd9aa7
Albie <albion@dragonfruit.network>
2021-09-07 19:56:51 +0100

    change to use new ranked icons

--- DragonFruit.Six.Api/Utils/SeasonalRanks.cs
+++ DragonFruit.Six.Api/Utils/SeasonalRanks.cs
@@ -55,104 +55,104 @@ namespace DragonFruit.Six.Api.Utils
 
         private static readonly RankInfo[] RankingV3 =
         {
-            new(0, "Unranked", "/rank/v3/0.svg", null, null),
+            new(0, "Unranked", "/rank/v3-png/0.png", null, null),
 
-            new(1, "Copper 5", "/rank/v3/1.svg", null, 1199),
-            new(2, "Copper 4", "/rank/v3/2.svg", 1200, 1299),
-            new(3, "Copper 3", "/rank/v3/3.svg", 1300, 1399),
-            new(4, "Copper 2", "/rank/v3/4.svg", 1400, 1499),
-            new(5, "Copper 1", "/rank/v3/5.svg", 1500, 1599),
+            new(1, "Copper 5", "/rank/v3-png/1.png", null, 1199),
+            new(2, "Copper 4", "/rank/v3-png/2.png", 1200, 1299),
+            new(3, "Copper 3", "/rank/v3-png/3.png", 1300, 1399),
+            new(4, "Copper 2", "/rank/v3-png/4.png", 1400, 1499),
+            new(5, "Copper 1", "/rank/v3-png/5.png", 1500, 1599),
 
-            new(6, "Bronze 5", "/rank/v3/6.svg", 1600, 1699),
-            new(7, "Bronze 4", "/rank/v3/7.svg", 1700, 1799),
-            new(8, "Bronze 3", "/rank/v3/8.svg", 1800, 1899),
-            new(9, "Bronze 2", "/rank/v3/9.svg", 1900, 1999),
-            new(10, "Bronze 1", "/rank/v3/10.svg", 2000, 2099),
+            new(6, "Bronze 5", "/rank/v3-png/6.png", 1600, 1699),
+            new(7, "Bronze 4", "/rank/v3-png/7.png", 1700, 1799),
+            new(8, "Bronze 3", "/rank/v3-png/8.png", 1800, 1899),
+            new(9, "Bronze 2", "/rank/v3-png/9.png", 1900, 1999),
+            new(10, "Bronze 1", "/rank/v3-png/10.png", 2000, 2099),
 
-            new(11, "Silver 5", "/rank/v3/11.svg", 2100, 2199),
-            new(12, "Silver 4", "/rank/v3/12.svg", 2200, 2299),
-            new(13, "Silver 3", "/rank/v3/13.svg", 2300, 2399),
-            new(14, "Silver 2", "/rank/v3/14.svg", 2400, 2499),
-            new(15, "Silver 1", "/rank/v3/15.svg", 2500, 2599),
+            new(11, "Silver 5", "/rank/v3-png/11.png", 2100, 2199),
+            new(12, "Silver 4", "/rank/v3-png/12.png", 2200, 2299),
+            new(13, "Silver 3", "/rank/v3-png/13.png", 2300, 2399),
+            new(14, "Silver 2", "/rank/v3-png/14.png", 2400, 2499),
+            new(15, "Silver 1", "/rank/v3-png/15.png", 2500, 2599),
 
-            new(16, "Gold 3", "/rank/v3/16.svg", 2600, 2799),
-            new(17, "Gold 2", "/rank/v3/17.svg", 2800, 2999),
-            new(18, "Gold 1", "/rank/v3/18.svg", 3000, 3199),
+            new(16, "Gold 3", "/rank/v3-png/16.png", 2600, 2799),
+            new(17, "Gold 2", "/rank/v3-png/17.png", 2800, 2999),
+            new(18, "Gold 1", "/rank/v3-png/18.png", 3000, 3199),
 
-            new(19, "Platinum 3", "/rank/v3/19.svg", 3200, 3499),
-            new(20, "Platinum 2", "/rank/v3/20.svg", 3500, 3799),
-            new(21, "Platinum 1", "/rank/v3/21.svg", 3800, 4099),
+            new(19, "Platinum 3", "/rank/v3-png/19.png", 3200, 3499),
+            new(20, "Platinum 2", "/rank/v3-png/20.png", 3500, 3799),
+            new(21, "Platinum 1", "/rank/v3-png/21.png", 3800, 4099),
 
-            new(22, "Diamond 3", "/rank/v3/22.svg", 4100, 4399),
-            new(23, "Diamond 2", "/rank/v3/23.svg", 4400, 4699),
-            new(24, "Diamond 1", "/rank/v3/24.svg", 4700, 4999),
+            new(22, "Diamond 3", "/rank/v3-png/22.png", 4100, 4399),
+            new(23, "Diamond 2", "/rank/v3-png/23.png", 4400, 4699),
+            new(24, "Diamond 1", "/rank/v3-png/24.png", 4700, 4999),
 
-            new(25, "Champion", "/rank/v3/25.svg", 5000, null)
+            new(25, "Champion", "/rank/v3-png/25.png", 5000, null)
         };
 
         private static readonly RankInfo[] RankingV2 =
         {
-            new(0, "Unranked", "/rank/v2/0.svg", null, null),
+            new(0, "Unranked", "/rank/v2-png/0.png", null, null),
 
-            new(1, "Copper 5", "/rank/v2/1.svg", null, 1199),
-            new(2, "Copper 4", "/rank/v2/2.svg", 1200, 1299),
-            new(3, "Copper 3", "/rank/v2/3.svg", 1300, 1399),
-            new(4, "Copper 2", "/rank/v2/4.svg", 1400, 1499),
-            new(5, "Copper 1", "/rank/v2/5.svg", 1500, 1599),
+            new(1, "Copper 5", "/rank/v2-png/1.png", null, 1199),
+            new(2, "Copper 4", "/rank/v2-png/2.png", 1200, 1299),
+            new(3, "Copper 3", "/rank/v2-png/3.png", 1300, 1399),
+            new(4, "Copper 2", "/rank/v2-png/4.png", 1400, 1499),
+            new(5, "Copper 1", "/rank/v2-png/5.png", 1500, 1599),
 
-            new(6, "Bronze 5", "/rank/v2/6.svg", 1600, 1699),
-            new(7, "Bronze 4", "/rank/v2/7.svg", 1700, 1799),
-            new(8, "Bronze 3", "/rank/v2/8.svg", 1800, 1899),
-            new(9, "Bronze 2", "/rank/v2/9.svg", 1900, 1999),
-            new(10, "Bronze 1", "/rank/v2/10.svg", 2000, 2099),
+            new(6, "Bronze 5", "/rank/v2-png/6.png", 1600, 1699),
+            new(7, "Bronze 4", "/rank/v2-png/7.png", 1700, 1799),
+            new(8, "Bronze 3", "/rank/v2-png/8.png", 1800, 1899),
+            new(9, "Bronze 2", "/rank/v2-png/9.png", 1900, 1999),
+            new(10, "Bronze 1", "/rank/v2-png/10.png", 2000, 2099),
 
-            new(11, "Silver 5", "/rank/v2/11.svg", 2100, 2199),
-            new(12, "Silver 4", "/rank/v2/12.svg", 2200, 2299),
-            new(13, "Silver 3", "/rank/v2/13.svg", 2300, 2399),
-            new(14, "Silver 2", "/rank/v2/14.svg", 2400, 2499),
-            new(15, "Silver 1", "/rank/v2/15.svg", 2500, 2599),
+            new(11, "Silver 5", "/rank/v2-png/11.png", 2100, 2199),
+            new(12, "Silver 4", "/rank/v2-png/12.png", 2200, 2299),
+            new(13, "Silver 3", "/rank/v2-png/13.png", 2300, 2399),
+            new(14, "Silver 2", "/rank/v2-png/14.png", 2400, 2499),
+            new(15, "Silver 1", "/rank/v2-png/15.png", 2500, 2599),
 
-            new(16, "Gold 3", "/rank/v2/16.svg", 2600, 2799),
-            new(17, "Gold 2", "/rank/v2/17.svg", 2800, 2999),
-            new(18, "Gold 1", "/rank/v2/18.svg", 3000, 3199),
+            new(16, "Gold 3", "/rank/v2-png/16.png", 2600, 2799),
+            new(17, "Gold 2", "/rank/v2-png/17.png", 2800, 2999),
+            new(18, "Gold 1", "/rank/v2-png/18.png", 3000, 3199),
 
-            new(19, "Platinum 3", "/rank/v2/19.svg", 3200, 3599),
-            new(20, "Platinum 2", "/rank/v2/20.svg", 3600, 3999),
-            new(21, "Platinum 1", "/rank/v2/21.svg", 4000, 4399),
+            new(19, "Platinum 3", "/rank/v2-png/19.png", 3200, 3599),
+            new(20, "Platinum 2", "/rank/v2-png/20.png", 3600, 3999),
+            new(21, "Platinum 1", "/rank/v2-png/21.png", 4000, 4399),
 
-            new(22, "Diamond", "/rank/v2/22.svg", 4400, 4999),
-            new(23, "Champion", "/rank/v2/23.svg", 5000, null)
+            new(22, "Diamond", "/rank/v2-png/22.png", 4400, 4999),
+            new(23, "Champion", "/rank/v2-png/23.png", 5000, null)
         };
 
         private static readonly RankInfo[] RankingV1 =
         {
-            new(0, "Unranked", "/rank/v1/0.svg", null, null),
+            new(0, "Unranked", "/rank/v1-png/0.png", null, null),
 
-            new(1, "Copper 4", "/rank/v1/1.svg", null, 1399),
-            new(2, "Copper 3", "/rank/v1/2.svg", 1400, 1499),
-            new(3, "Copper 2", "/rank/v1/3.svg", 1500, 1599),
-            new(4, "Copper 1", "/rank/v1/4.svg", 1600, 1699),
+            new(1, "Copper 4", "/rank/v1-png/1.png", null, 1399),
+            new(2, "Copper 3", "/rank/v1-png/2.png", 1400, 1499),
+            new(3, "Copper 2", "/rank/v1-png/3.png", 1500, 1599),
+            new(4, "Copper 1", "/rank/v1-png/4.png", 1600, 1699),
 
-            new(5, "Bronze 4", "/rank/v1/5.svg", 1700, 1799),
-            new(6, "Bronze 3", "/rank/v1/6.svg", 1800, 1899),
-            new(7, "Bronze 2", "/rank/v1/7.svg", 1900, 1999),
-            new(8, "Bronze 1", "/rank/v1/8.svg", 2000, 2099),
+            new(5, "Bronze 4", "/rank/v1-png/5.png", 1700, 1799),
+            new(6, "Bronze 3", "/rank/v1-png/6.png", 1800, 1899),
+            new(7, "Bronze 2", "/rank/v1-png/7.png", 1900, 1999),
+            new(8, "Bronze 1", "/rank/v1-png/8.png", 2000, 2099),
 
-            new(9, "Silver 4", "/rank/v1/9.svg", 2100, 2199),
-            new(10, "Silver 3", "/rank/v1/10.svg", 2200, 2299),
-            new(11, "Silver 2", "/rank/v1/11.svg", 2300, 2399),
-            new(12, "Silver 1", "/rank/v1/12.svg", 2400, 2499),
+            new(9, "Silver 4", "/rank/v1-png/9.png", 2100, 2199),
+            new(10, "Silver 3", "/rank/v1-png/10.png", 2200, 2299),
+            new(11, "Silver 2", "/rank/v1-png/11.png", 2300, 2399),
+            new(12, "Silver 1", "/rank/v1-png/12.png", 2400, 2499),
 
-            new(13, "Gold 4", "/rank/v1/13.svg", 2500, 2699),
-            new(14, "Gold 3", "/rank/v1/14.svg", 2700, 2899),
-            new(15, "Gold 2", "/rank/v1/15.svg", 2900, 3099),
-            new(16, "Gold 1", "/rank/v1/16.svg", 3100, 3299),
+            new(13, "Gold 4", "/rank/v1-png/13.png", 2500, 2699),
+            new(14, "Gold 3", "/rank/v1-png/14.png", 2700, 2899),
+            new(15, "Gold 2", "/rank/v1-png/15.png", 2900, 3099),
+            new(16, "Gold 1", "/rank/v1-png/16.png", 3100, 3299),
 
-            new(17, "Platinum 3", "/rank/v1/17.svg", 3300, 3699),
-            new(18, "Platinum 2", "/rank/v1/18.svg", 3700, 4099),
-            new(19, "Platinum 1", "/rank/v1/19.svg", 4100, 4499),
+            new(17, "Platinum 3", "/rank/v1-png/17.png", 3300, 3699),
+            new(18, "Platinum 2", "/rank/v1-png/18.png", 3700, 4099),
+            new(19, "Platinum 1", "/rank/v1-png/19.png", 4100, 4499),
 
-            new(20, "Diamond", "/rank/v1/20.svg", 4500, null)
+            new(20, "Diamond", "/rank/v1-png/20.png", 4500, null)
         };
 
         #endregion
