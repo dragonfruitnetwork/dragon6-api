9bed631268d4cdf3a2b6d0b63fd5a5908359b7df
Albie <albion@dragonfruit.network>
2022-03-24 21:03:27 +0000

    add tests

--- DragonFruit.Six.Api.Tests/Data/SeasonalStatsTests.cs
+++ DragonFruit.Six.Api.Tests/Data/SeasonalStatsTests.cs
@@ -41,5 +41,15 @@ namespace DragonFruit.Six.Api.Tests.Data
             Assert.IsNotNull(whiteNoise.Name);
             Assert.AreEqual(2, whiteNoise.Year);
         }
+
+        [TestCase("14c01250-ef26-4a32-92ba-e04aa557d619", Platform.PC, 17, 17)]
+        public async Task TestSeasonalRecords(string identifier, Platform platform, int season15Rank, int season21Rank)
+        {
+            var account = await GetAccountInfoFor(identifier, platform);
+            var seasons = Enumerable.Range(15, 7);
+            var stats = (await Client.GetSeasonalStatsRecordsAsync(account, seasons, BoardType.Ranked, Region.EMEA)).ToArray();
+
+            Assert.AreEqual(season15Rank, stats.SingleOrDefault(x => x.SeasonId == 15)?.Rank);
+        }
     }
 }
