922b593a9e3b25f1d29fb3ae1bac58b923ffe06b
Albie <albion@dragonfruit.network>
2022-04-24 09:45:47 +0100

    add [Column] annotations to operator info

--- DragonFruit.Six.Api/Modern/Entities/ModernOperatorInfo.cs
+++ DragonFruit.Six.Api/Modern/Entities/ModernOperatorInfo.cs
@@ -2,6 +2,7 @@
 // Licensed under Apache-2. Refer to the LICENSE file for more info
 
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using DragonFruit.Six.Api.Enums;
 using Newtonsoft.Json;
 
@@ -16,42 +17,49 @@ namespace DragonFruit.Six.Api.Modern.Entities
         /// <summary>
         /// Operator Identifier
         /// </summary>
+        [Column("id")]
         [JsonProperty("id")]
         public string OperatorId { get; set; }
 
         /// <summary>
         /// Operator name
         /// </summary>
+        [Column("name")]
         [JsonProperty("name")]
         public string Name { get; set; }
 
         /// <summary>
         /// Numeric value representing the relative release order of the operator
         /// </summary>
+        [Column("order")]
         [JsonProperty("ord")]
         public ushort Order { get; set; }
 
         /// <summary>
         /// Fully qualified operator icon url
         /// </summary>
+        [Column("img")]
         [JsonProperty("img")]
         public string ImageUrl { get; set; }
 
         /// <summary>
         /// The name of the organisation the operator is from
         /// </summary>
+        [Column("org")]
         [JsonProperty("org")]
         public string Organisation { get; set; }
 
         /// <summary>
         /// The subtitle, as seen underneath the operator's name in game
         /// </summary>
+        [Column("sub")]
         [JsonProperty("sub")]
         public string Subtitle { get; set; }
 
         /// <summary>
         /// The operator's use, attacker/defender
         /// </summary>
+        [Column("type")]
         [JsonProperty("type")]
         public OperatorType Type { get; set; }
     }
