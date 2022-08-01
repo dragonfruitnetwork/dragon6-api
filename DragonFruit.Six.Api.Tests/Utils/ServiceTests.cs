6473472754a060c028540d24351acba10941897e
Albie <albion@dragonfruit.network>
2022-08-01 12:41:37 +0100

    add IPv6 address to test

--- DragonFruit.Six.Api.Tests/Utils/ServiceTests.cs
+++ DragonFruit.Six.Api.Tests/Utils/ServiceTests.cs
@@ -19,15 +19,18 @@ namespace DragonFruit.Six.Api.Tests.Utils
             // as we don't know who's running this, there's no real way to validate correctness of this part
             await Client.GeolocateAsync().ConfigureAwait(false);
 
-            var addressInfo = await Client.GeolocateAsync("8.8.8.8", "172.217.16.238", "194.39.167.145").ConfigureAwait(false);
+            var addressInfo = await Client.GeolocateAsync("8.8.8.8", "172.217.16.238", "194.39.167.145", "2620:119:35::35").ConfigureAwait(false);
             var addressDirectory = addressInfo.ToDictionary(x => x.IP);
 
             // google services
-            Assert.IsTrue(addressDirectory["8.8.8.8"].ServiceProvider == "Google");
-            Assert.IsTrue(addressDirectory["172.217.16.238"].ServiceProvider == "Google Servers");
+            Assert.AreEqual("Google", addressDirectory["8.8.8.8"].ServiceProvider);
+            Assert.AreEqual("Google Servers", addressDirectory["172.217.16.238"].ServiceProvider);
+
+            // IPv6 DNS server
+            Assert.AreEqual("Cisco OpenDNS, LLC", addressDirectory["2620:119:35::35"].ServiceProvider);
 
             // random ip address from UK
-            Assert.IsTrue(addressDirectory["194.39.167.145"].CountryName == "United Kingdom");
+            Assert.AreEqual("United Kingdom", addressDirectory["194.39.167.145"].CountryName);
         }
 
         [Test]
