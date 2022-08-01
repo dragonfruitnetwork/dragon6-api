54d9f0a21ef6a0b81d05eb66a293d9fecbb18957
Albie <albion@dragonfruit.network>
2022-08-01 12:28:10 +0100

    add own extensions and add request listed in #321

---
+++ DragonFruit.Six.Api/Services/Geolocation/Requests/UbisoftGeolocationRequest.cs
@@ -0,0 +1,25 @@
+﻿// Dragon6 API Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under Apache-2. Refer to the LICENSE file for more info
+
+using System.Collections.Generic;
+using DragonFruit.Data;
+using DragonFruit.Data.Parameters;
+
+namespace DragonFruit.Six.Api.Services.Geolocation.Requests
+{
+    /// <summary>
+    /// Creates a request to get the approximate location a collection of provided IP addresses
+    /// </summary>
+    public class UbisoftGeolocationRequest : UbiApiRequest
+    {
+        public override string Path => $"{Endpoints.BaseEndpoint}/v2/iplocation";
+
+        public UbisoftGeolocationRequest(IEnumerable<string> addresses)
+        {
+            Addresses = addresses;
+        }
+
+        [QueryParameter("ips", CollectionConversionMode.Concatenated)]
+        public IEnumerable<string> Addresses { get; }
+    }
+}
