cac0deabed5ac0306b1021f0b83e4eec4727993f
Albie <albion@dragonfruit.network>
2022-12-22 17:51:53 +0000

    add new level request/response models

---
+++ DragonFruit.Six.Api/Accounts/Entities/AccountLevel.cs
@@ -0,0 +1,16 @@
+﻿// Dragon6 API Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under Apache-2. Refer to the LICENSE file for more info
+
+using Newtonsoft.Json;
+
+namespace DragonFruit.Six.Api.Accounts.Entities
+{
+    public class AccountLevel
+    {
+        [JsonProperty("level")]
+        public int Level { get; set; }
+
+        [JsonProperty("xp")]
+        public int XP { get; set; }
+    }
+}
---
+++ DragonFruit.Six.Api/Accounts/Requests/AccountLevelRequest.cs
@@ -0,0 +1,24 @@
+﻿// Dragon6 API Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under Apache-2. Refer to the LICENSE file for more info
+
+using DragonFruit.Data.Parameters;
+using DragonFruit.Six.Api.Accounts.Entities;
+using DragonFruit.Six.Api.Accounts.Enums;
+
+namespace DragonFruit.Six.Api.Accounts.Requests
+{
+    public class AccountLevelRequest : UbiApiRequest
+    {
+        public override string Path => Platform.CrossPlatform.SpaceUrl(1) + "/title/r6s/rewards/public_profile";
+
+        public AccountLevelRequest(UbisoftAccount account)
+        {
+            Account = account;
+        }
+
+        public UbisoftAccount Account { get; }
+
+        [QueryParameter("profile_id")]
+        private string ProfileId => Account.ProfileId;
+    }
+}
