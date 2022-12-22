dbf5cf20ea6cf4f7191492db8629b0cfbf44b140
Albie <albion@dragonfruit.network>
2022-12-22 14:37:26 +0000

    fix seasonal stats tests

--- DragonFruit.Six.Api.Tests/Data/SeasonalStatsTests.cs
+++ DragonFruit.Six.Api.Tests/Data/SeasonalStatsTests.cs
@@ -13,35 +13,6 @@ namespace DragonFruit.Six.Api.Tests.Data
     [TestFixture]
     public class SeasonalStatsTests : Dragon6ApiTest
     {
-        [TestCase("14c01250-ef26-4a32-92ba-e04aa557d619", Platform.PC, 17, 17)]
-        public async Task TestSeasonalStats(string identifier, Platform platform, int season, int maxRank)
-        {
-            var account = await GetAccountInfoFor(identifier, platform);
-            var stats = await Client.GetSeasonalStatsAsync(account, season);
-
-            Assert.AreEqual(maxRank, stats.MaxRank);
-        }
-
-        [TestCase("14c01250-ef26-4a32-92ba-e04aa557d619", Platform.PC, Region.EMEA, 10, 13)]
-        public async Task TestLegacySeasonalStats(string identifier, Platform platform, Region region, int season, int maxRank)
-        {
-            var account = await GetAccountInfoFor(identifier, platform);
-            var stats = await Client.GetSeasonalStatsAsync(account, season, region: region);
-
-            Assert.AreEqual(maxRank, stats.MaxRank);
-        }
-
-        [Test]
-        public async Task TestSeasonInformation()
-        {
-            var seasons = await Client.GetSeasonInfoAsync();
-            var whiteNoise = seasons.SingleOrDefault(x => x.Id == 8);
-
-            Assert.IsNotNull(whiteNoise);
-            Assert.IsNotNull(whiteNoise.Name);
-            Assert.AreEqual(2, whiteNoise.Year);
-        }
-
         [TestCase("14c01250-ef26-4a32-92ba-e04aa557d619", Platform.PC, 0, 16)]
         public async Task TestSeasonalRecords(string identifier, Platform platform, int season15Rank, int season17Rank)
         {
@@ -51,6 +22,29 @@ namespace DragonFruit.Six.Api.Tests.Data
 
             Assert.AreEqual(season15Rank, stats[15].Rank);
             Assert.AreEqual(season17Rank, stats[17].Rank);
+        }
+
+        /// <summary>
+        /// Tests seasonal lookup against ranked2 and older seasons
+        /// </summary>
+        [Test]
+        public async Task TestMultiPlatformMultiVersionRanked()
+        {
+            var accounts = new[]
+            {
+                await GetAccountInfoFor("14c01250-ef26-4a32-92ba-e04aa557d619", Platform.PC),
+                await GetAccountInfoFor("45c0cccb-a1a8-4433-b3d8-52aaa40d16d2", Platform.PC),
+                await GetAccountInfoFor("5e992dc8-7d93-4f28-9690-08b4d6788cc8", Platform.PSN)
+            };
+
+            var stats = await Client.GetSeasonalStatsRecordsAsync(accounts, new[] { 28, 27, 26, 25 });
+            Assert.AreEqual(48, stats.Count);
+
+            var casualMMR = stats.Single(x => x.ProfileId == "14c01250-ef26-4a32-92ba-e04aa557d619" && x.Board == BoardType.Casual && x.SeasonId == 26).MMR;
+            Assert.Greater(casualMMR, 2200);
+
+            var rankedWins = stats.Where(x => x.ProfileId == "45c0cccb-a1a8-4433-b3d8-52aaa40d16d2" && x.Board == BoardType.Ranked).OrderByDescending(x => x.SeasonId).First().Wins;
+            Assert.Greater(rankedWins, 5);
         }
     }
 }
