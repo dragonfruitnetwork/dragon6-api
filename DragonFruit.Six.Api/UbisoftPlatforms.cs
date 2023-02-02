57636c5fa947dfd0d2d304f50608d2722eda9ddb
Albie <albion@dragonfruit.network>
2023-02-02 13:10:12 +0000

    add const platform strings

---
+++ DragonFruit.Six.Api/UbisoftPlatforms.cs
@@ -0,0 +1,16 @@
+// Dragon6 API Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under Apache-2. Refer to the LICENSE file for more info
+
+// ReSharper disable InconsistentNaming
+
+namespace DragonFruit.Six.Api
+{
+    public static class UbisoftPlatforms
+    {
+        public const string PC = "uplay";
+        public const string XBOX = "xbl";
+        public const string PLAYSTATION = "psn";
+
+        public const string CROSSPLAY = "xplay";
+    }
+}
